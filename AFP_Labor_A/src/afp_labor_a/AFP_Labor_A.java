674599d5b7a1edefb9a6c922ba1a7e82f9725684
tuzesbalazs2 <tuzes.balazs.2@gmail.com>
2019-11-15 00:29:46 +0100

    afplabor 2018.12.14. 0:50 verzió

---
+++ AFP_Labor_A/src/afp_labor_a/AFP_Labor_A.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package afp_labor_a;
+
+/**
+ *
+ * @author Tóth_Zsolt
+ */
+public class AFP_Labor_A {
+    
+    
+
+    public static final String DB_ADDRESS = "jdbc:mysql://85.10.205.173:3306/afplabor";
+    public static final String DB_NAME = "afplabor";
+    public static final String DB_USERNAME = "zsolt1";
+    public static final String DB_PASSWORD = "abcdefgh";
+    
+    public static Database db = new Database(DB_ADDRESS, DB_NAME,
+            DB_USERNAME, DB_PASSWORD);
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainPage m = new MainPage();
+        m.setVisible(true);
+        
+        
+    }
+    
+}
