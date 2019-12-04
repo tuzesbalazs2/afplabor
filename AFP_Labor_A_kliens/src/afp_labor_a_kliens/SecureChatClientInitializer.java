69a20b185e93e478b0ae71f6416c82ee8ce681ed
tuzesbalazs2 <tuzes.balazs.2@gmail.com>
2019-12-04 23:05:53 +0100

    Szerver és Kliens kód módosítása

---
+++ AFP_Labor_A_kliens/src/afp_labor_a_kliens/SecureChatClientInitializer.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package afp_labor_a_kliens;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.Delimiters;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.ssl.SslContext;
+
+/**
+ *
+ * @author Tüzes
+ */
+
+/**
+ * Creates a newly configured {@link ChannelPipeline} for a new channel.
+ */
+public class SecureChatClientInitializer extends ChannelInitializer<SocketChannel> {
+
+    private final SslContext sslCtx;
+
+    public SecureChatClientInitializer(SslContext sslCtx) {
+        this.sslCtx = sslCtx;
+    }
+
+    @Override
+    public void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        // Add SSL handler first to encrypt and decrypt everything.
+        // In this example, we use a bogus certificate in the server side
+        // and accept any invalid certificates in the client side.
+        // You will need something more complicated to identify both
+        // and server in the real world.
+        pipeline.addLast(sslCtx.newHandler(ch.alloc(), SecureChatClient.HOST, SecureChatClient.PORT));
+
+        // On top of the SSL handler, add the text line codec.
+        pipeline.addLast(new DelimiterBasedFrameDecoder(8192, Delimiters.lineDelimiter()));
+        pipeline.addLast(new StringDecoder());
+        pipeline.addLast(new StringEncoder());
+
+        // and then business logic.
+        pipeline.addLast(new SecureChatClientHandler());
+    }
+}
+
