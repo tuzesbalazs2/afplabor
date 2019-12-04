d5b9827e5b0578a6543e9e59e45a78b396dffc22
tuzesbalazs2 <tuzes.balazs.2@gmail.com>
2019-12-04 17:36:25 +0100

    Szerver fájlok módosítása

---
+++ AFP_Labor_A/src/afp_labor_a/DiscardServerHandler.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package afp_labor_a;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+/**
+ *
+ * @author Tüzes
+ */
+
+
+/**
+ * Handles a server-side channel.
+ */
+public class DiscardServerHandler extends SimpleChannelInboundHandler<Object> {
+
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+        // discard
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
